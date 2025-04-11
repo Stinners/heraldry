bea44049c8f81aaf3e4d624a4d5f30f4bce7dbdd
Chris Stinson <chris.stinson202@gmail.com>
2025-04-11 21:32:26 +1200

    Parsing very basic select statements

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,55 +1,96 @@
-
 #![allow(dead_code)]
 
 use std::iter::Peekable;
-use unicode_segmentation::{Graphemes, UnicodeSegmentation};
+use std::str::Chars;
 use std::sync::Arc;
-
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Loc {
     start: usize,
     end: usize,
-    line: usize,
+    line: [usize; 2],
+    line_start: [usize; 2],
 }
 
 impl Loc {
     fn new() -> Self {
         Loc {
+            // The first byte of the span 
             start: 0,
+
+            // The final byte of the span 
             end: 0,
-            line: 1,
+
+            // The line the start and end cursors are one
+            line: [1, 1],
+
+            // The byte where the line starts
+            line_start: [0,0],
         }
     }
 
-    fn advance(&mut self, cluster: &str) {
-        self.end = self.end + cluster.len()
+    // Length of the span in bytes - not in chars
+    fn len(&self) -> usize {
+        self.end - self.start
+    }
+
+    fn advance(&mut self, c: char) {
+        self.end += c.len_utf8();
+        if c == '\n' {
+            // Advance the end
+            self.line[1] += 1;
+            self.line_start[1] = self.end + c.len_utf8();
+        }
+    }
+
+    #[inline(always)]
+    fn debug_assert_zero_length(&self) {
+        debug_assert!(self.start == self.end);
+        debug_assert!(self.line[0] == self.line[1]);
+        debug_assert!(self.line_start[0] == self.line_start[1]);
+        debug_assert!(self.len() == 0);
+    }
+
+    fn advance_start(&mut self, c: char) {
+        let step = c.len_utf8();
+        self.start += step;
+        if c == '\n' {
+            self.line[0] += 1;
+            self.line_start[0] += step;
+        }
+        self.debug_assert_zero_length();
+    }
+
+    fn start_new(&self) -> Self {
+        let mut new_span = self.clone();
+        new_span.start = new_span.end;
+        new_span.line[0] = new_span.line[1];
+        new_span.line_start[0] = new_span.line_start[1];
+        new_span
     }
 }
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum TokenT {
     // Single word tokens
-    SELECT, FROM,
+    SELECT,
+    FROM,
 
     // Symbols
-    STAR, SEMICOLON,
+    STAR,
+    SEMICOLON, COMMA,
 
     // Identifer
     IDENTIFIER,
 }
 
-const KEYWORD_MAP: &[(&str, TokenT)] = 
-    &[("select", TokenT::SELECT),
-      ("from", TokenT::FROM),
-    ];
-
+const KEYWORD_MAP: &[(&str, TokenT)] = &[("select", TokenT::SELECT), ("from", TokenT::FROM)];
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct Token {
-    kind: TokenT,
-    span: Loc,
-    text: Arc<str>
+    pub kind: TokenT,
+    pub span: Loc,
+    pub text: Arc<str>,
 }
 
 impl Token {
@@ -58,53 +99,48 @@ impl Token {
     }
 }
 
+fn identifier_start(c: char) -> bool {
+    c.is_alphabetic() || c == '_'
+}
+
+fn identifier_char(c: char) -> bool {
+    c.is_alphabetic() || c.is_numeric() || c == '_'
+}
 
 #[derive(Debug)]
 pub struct Lexer<'a> {
-    graphs: Peekable<Graphemes<'a>>,
-    text: Arc<str>,
-    span: Loc,
+    chars: Peekable<Chars<'a>>,
+    pub text: Arc<str>,
+    pub span: Loc,
 }
 
 impl<'a> Lexer<'a> {
-    fn lex_string(string: &'a str) -> Self {
-        Lexer {
-            graphs: UnicodeSegmentation::graphemes(string, true).peekable(),
-            text: string.into(),
-            span: Loc::new(),
-        }
-    }
-
     fn get_text(&self) -> &str {
         &self.text[self.span.start..self.span.end]
     }
 
-    fn emit(& mut self, kind: TokenT) -> Token {
+    fn emit(&mut self, kind: TokenT) -> Token {
         let old_span = self.span;
-        self.span.start = self.span.end;
+        self.span = old_span.start_new();
         Token {
             kind,
             span: old_span,
-            text: self.text.clone()
+            text: self.text.clone(),
         }
     }
 
-    fn advance(&mut self) -> Option<&str> {
-        match self.graphs.next() {
-            Some(cluster) => {
-                self.span.advance(cluster);
-                Some(cluster)
-            },
-            None => None,
-        }
+    fn advance(&mut self) -> Option<char> {
+        self.chars.next().map(|c| {
+            self.span.advance(c);
+            c
+        })
     }
 
-
-    fn peek(&mut self) -> Option<&str> {
-        self.graphs.peek().map(|c| &**c)
+    fn peek(&mut self) -> Option<char> {
+        self.chars.peek().copied()
     }
 
-    // We could do this faster with a trie 
+    // There are faster ways to do this
     fn keyword_type(&self) -> TokenT {
         let text = self.get_text();
         for (word, token_t) in KEYWORD_MAP.iter() {
@@ -115,22 +151,44 @@ impl<'a> Lexer<'a> {
         TokenT::IDENTIFIER
     }
 
+    fn consume_identifier(&mut self) {
+        while let Some(c) = self.peek() {
+            if !identifier_char(c) {
+                break;
+            }
+            self.advance();
+        }
+    }
 
     pub fn lex(&mut self) -> Option<Token> {
-        let next_c = self.advance();
-        if next_c.is_none() {
-            return None;
-        }
+        // Loop until we either consume leading whitespace or reach the end of the file
+        let next_c = loop {
+            if let Some(next_c) = self.advance() {
+                if !next_c.is_whitespace() {
+                    break next_c;
+                }
+                else {
+                    self.span.advance_start(next_c);
+                }
+            } 
+            // End of file
+            else {
+                
+                return None;
+            }
+        };
 
-        let token = match next_c.unwrap() {
-            // Single character Tokens 
-            "*" => self.emit(TokenT::STAR),
-            ";" => self.emit(TokenT::SEMICOLON),
+        let token = match next_c {
+            // Single character Tokens
+            '*' => self.emit(TokenT::STAR),
+            ';' => self.emit(TokenT::SEMICOLON),
+            ',' => self.emit(TokenT::COMMA),
 
-            // Double Character Tokens 
+            // Double Character Tokens
 
-            // Keywords and identifiers 
+            // Keywords and identifiers
             _keyword_or_identifier => {
+                self.consume_identifier();
                 let token_t = self.keyword_type();
                 self.emit(token_t)
             }
@@ -148,8 +206,54 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
+impl<'a> From<&'a str> for Lexer<'a> {
+    fn from(text: &'a str) -> Lexer<'a> {
+        Lexer {
+            chars: text.chars().peekable(),
+            text: text.into(),
+            span: Loc::new(),
+        }
+    }
+}
 
-#[cfg(test)] 
+#[cfg(test)]
 mod tests {
     use super::*;
+    use TokenT::*;
+
+    fn make_tokens(text: &str) -> Vec<Token> {
+        Lexer::from(text).into_iter().collect()
+    }
+
+    fn compare_kinds(expected: &[TokenT], actual: Vec<Token>) {
+        assert_eq!(expected.len(), actual.len());
+
+        for (act, exp) in expected.iter().zip(actual) {
+            assert_eq!(*act, exp.kind);
+        }
+    }
+
+    #[test]
+    fn handles_empty_string_properly() {
+        let tokens = make_tokens("");
+        assert!(tokens.len() == 0);
+    }
+
+    #[test]
+    fn lex_keywords() {
+        let tokens = make_tokens("  select  SELECT From foo ");
+        compare_kinds(&[SELECT, SELECT, FROM, IDENTIFIER], tokens);
+    }
+
+    #[test]
+    fn lex_punctuation() {
+        let tokens = make_tokens("*; ,");
+        compare_kinds(&[STAR, SEMICOLON, COMMA], tokens);
+    }
+
+    #[test]
+    fn lex_simple_select_punctuation() {
+        let tokens = make_tokens("select *, bar from foo;");
+        compare_kinds(&[SELECT, STAR, COMMA, IDENTIFIER, FROM, IDENTIFIER, SEMICOLON], tokens);
+    }
 }
---
+++ src/parser.rs
@@ -0,0 +1,220 @@
+#![allow(dead_code)]
+
+use std::iter::Peekable;
+
+use crate::lexer::{Token, Loc, Lexer, TokenT};
+
+#[derive(Debug)]
+enum Expression {
+    IDENT(Token),
+}
+
+#[derive(Debug)]
+enum Projection {
+    STAR(Loc), 
+    EXPRESSION(Expression),
+}
+
+#[derive(Debug)]
+struct SelectStmt {
+    projs:Vec<Projection>,
+    table: Expression,
+    span: Loc,
+}
+
+#[derive(Debug)]
+enum ParseErrorT {
+    EOF, 
+    UnexpectedToken,
+}
+
+#[derive(Debug)]
+struct ParseError {
+    msg: String,
+    span: Loc,
+    kind: ParseErrorT,
+}
+
+impl ParseError {
+    fn eof<R>(loc: Loc) -> Result<R, Self> {
+        Err(ParseError {
+            msg: "Unexpected end of file".into(),
+            span: loc,
+            kind: ParseErrorT::EOF
+
+        })
+    }
+
+    fn unexpected<R>(expected: TokenT, actual: Token) -> Result<R, Self> {
+        Err(ParseError {
+            msg: format!("Expected {:?}, but got {:?}", expected, actual.kind),
+            span: actual.span,
+            kind: ParseErrorT::UnexpectedToken,
+        })
+    }
+
+    fn elaborate(&mut self, text: &str) {
+        self.msg.push_str(" ");
+        self.msg.push_str(text);
+    }
+
+    fn replace(mut self, text: &str) -> ParseError {
+        self.msg = text.to_string();
+        self
+    }
+}
+
+struct ParserSettings {
+    allow_trailing_commas: bool,
+}
+
+pub struct Parser<'a> {
+    lexer: Peekable<Lexer<'a>>,
+    settings: ParserSettings,
+    span: Loc,
+}
+
+impl<'a> Parser<'a> {
+
+    fn new(lexer: Lexer<'a>, settings: ParserSettings) -> Self {
+        Parser {
+            span: lexer.span,
+            lexer: lexer.peekable(),
+            settings,
+        }
+    }
+
+    // =============== Helper Functions =============== */
+
+    fn next(&mut self) -> Result<Token, ParseError> {
+        match self.lexer.next() {
+            Some(token) => {
+                self.span = token.span;
+                Ok(token)
+            }
+            None => ParseError::eof(self.span)
+        }
+    }
+
+    fn peek(&mut self) -> Option<&Token> {
+        self.lexer.peek()
+    }
+
+    // Advances only of the token matches
+    fn match_token(&mut self, token_type: TokenT) -> Option<Token> {
+        match self.peek() {
+            Some(token) if token.kind == token_type => {
+                Some(self.next().unwrap())
+            }
+            _ => None
+        }
+    }
+
+    fn match_one_of(&mut self, token_types: &[TokenT]) -> Option<Token> {
+        match self.peek() {
+            None => None,
+            Some(token) => {
+                for token_type in token_types {
+                    if token.kind == *token_type {
+                        return Some(self.next().unwrap());
+                    }
+                }
+                None
+            }
+        }
+    }
+
+
+    // TODO, these functions should peek and only consume if they match
+    fn require_token(&mut self, token_type: TokenT) -> Result<Token, ParseError> {
+        match self.peek() {
+            None => ParseError::eof(self.span),
+            Some(token) if token.kind == token_type => {
+                self.next()
+            }
+            Some(token) => ParseError::unexpected(token_type, token.clone())
+        }
+    }
+
+
+    fn require_one_of(&mut self, token_types: &[TokenT]) -> Result<Token, ParseError> {
+        match self.peek() {
+            None => ParseError::eof(self.span),
+            Some(token) => {
+                if token_types.iter().any(|t| *t == token.kind) {
+                    self.next()
+                }
+                else {
+                    Err(ParseError {
+                        msg: format!("Expected one of {:?}, but got {:?}", token_types, token.kind),
+                        span: token.span,
+                        kind: ParseErrorT::UnexpectedToken,
+                    })
+                }
+            }
+        }
+    }
+
+    // =============== Parsing Functions =============== */
+
+    fn parse_projection(&mut self) -> Result<Projection, ParseError> {
+        let token = self.require_one_of(&[TokenT::STAR, TokenT::IDENTIFIER])?;
+
+        Ok(match token.kind {
+            TokenT::STAR => Projection::STAR(token.span),
+            TokenT::IDENTIFIER => Projection::EXPRESSION(Expression::IDENT(token)),
+            _ => unreachable!("Unexpected token type in parse_projection")
+        })
+    }
+
+    fn parse_projections(&mut self) -> Result<Vec<Projection>, ParseError> {
+        let mut projections = vec!();
+
+        // We must parse at least one projection 
+        match self.parse_projection() {
+            Ok(proj) => projections.push(proj),
+            Err(mut err) => {
+                err.elaborate("Expected at least one projection");
+                return Err(err);
+            }
+        }
+
+        // Then we can parse any number of commas followed by projections
+        loop {
+            // Consume a comma and break if it's not one 
+            if self.match_token(TokenT::COMMA).is_none() {
+                break;
+            }
+
+            match self.parse_projection() {
+                Ok(proj) => projections.push(proj),
+                Err(_) if self.settings.allow_trailing_commas => {
+                    break;
+                },
+                Err(err) => { return Err(err) }
+            }
+        }
+        Ok(projections)
+    }
+
+    fn parse_from(&mut self) -> Result<Expression, ParseError> {
+        let _ = self.require_token(TokenT::FROM)?;
+        let table = self.require_token(TokenT::IDENTIFIER)?;
+        Ok(Expression::IDENT(table))
+    }
+
+    fn parse_select(&mut self) -> Result<SelectStmt, ParseError> {
+        let span = self.span;
+        let _ = self.require_token(TokenT::SELECT)?;
+        let projections = self.parse_projections()?;
+        let from = self.parse_from()?;
+
+        Ok(SelectStmt {
+            projs: projections,
+            table: from,
+            span
+        })
+
+    }
+}
+
